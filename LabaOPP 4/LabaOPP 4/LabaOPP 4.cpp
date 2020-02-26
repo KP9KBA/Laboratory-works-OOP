79a997825261aba9fd66de7d836dd98f4b8dd985
KP9KBA <55097325+KP9KBA@users.noreply.github.com>
2020-02-26 13:37:45 +0300

    структура

---
+++ LabaOPP 4/LabaOPP 4/LabaOPP 4.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <iomanip>//for setw()
+#include <conio.h>//for getche()
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Введите номер задания лабораторной работы: ";
+	cin >> a;
+	switch (a) {
+	case 1: {}break;
+	}
+}
